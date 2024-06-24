9433ece8f31b1b5580a5d6f0d9e9213b8b537562
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-06-24 16:26:49 -0300

    fede fijate si te llega

---
+++ tpomongoredis/src/main/java/com/example/demo/TpomongoredisApplication.java
@@ -0,0 +1,167 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.modelo.Cliente;
+import com.example.demo.modelo.Detalle;
+import com.example.demo.modelo.Producto;
+import com.example.demo.service.AdminService;
+import com.example.demo.service.ClienteService;
+import com.example.demo.service.ProductoService;
+
+@SpringBootApplication
+public class TpomongoredisApplication implements CommandLineRunner {
+
+	@Autowired
+	AdminService adminService;
+
+	@Autowired
+	ProductoService productoService;
+
+	@Autowired
+	ClienteService clienteService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpomongoredisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// SOLO SE USA PARA TESTEAR
+//		 // Verificar la conexión a Redis
+//       try {
+//           redisTemplate.opsForValue().set("testKey", "testValue");
+//           String value = redisTemplate.opsForValue().get("testKey");
+//           System.out.println("Conexión establecida con Redis. Valor recuperado: " + value);
+//       } catch (Exception e) {
+//           System.err.println("Error al conectar con Redis: " + e.getMessage());
+//       }
+
+// ----------------------------- ADMIN -----------------------------
+
+//		Agregar un Admin
+//		String mail = "ignacio8@gmail.com";
+//		String password = "nacho";
+//		String nombre = "Ignacio";
+//		String apellido = "Dorigo";
+//		String documento = "42414241";
+//		System.out.println(adminService.registerAdmin(mail, password, nombre, apellido, documento));
+
+//		Eliminar un Admin
+//		String mail = "nacho@gmail.com";
+//		String password = "nacho";
+//		System.out.println(adminService.eliminarAdmin(mail));
+
+//		Login Admin
+//		String mail = "nacho@gmail.com";
+//		String password = "nacho";
+//		System.out.println(adminService.loginAdmin(mail, password));
+
+//		Olvide mi contrasenia
+//		String mail = "nacho@gmail.com";
+//		System.out.println(adminService.olvideContrasena(mail));
+
+//		Cambiar contrasenia
+//		String mail = "nacho@gmail.com";
+//		String password = "nacho";
+//		String nueva1 = "uade";
+//		String nueva2 = "uade";
+//		System.out.println(adminService.cambiarContraseniaAdmin(mail, password, nueva1, nueva2));
+
+// ----------------------------- PRODUCTOS ----------------------------- 
+
+//		Agregar Producto
+//		Integer id = 8;
+//		String nombre = "Iphone 1qq52 pro max";
+//		Double precio = 1259.0;
+//		Integer stock = 100;
+//		String imagen = "https://http2.mlstatic.com/D_Q_NP_924631-MLA71783367058_092023-O.webp";
+//		System.out.println(productoService.agregarProducto(id, nombre, precio, stock, imagen));
+
+//		Eliminar Producto
+//		Integer id = 2;
+//		System.out.println(productoService.eliminarProducto(id));
+
+//		Modificar Precio
+//		Integer id = 1;
+//		Double precioNuevo = 666.0;
+//		System.out.println(productoService.modificarPrecio(id, precioNuevo));
+
+//		Modificar Stock
+//		Integer id = 1;
+//		Integer nuevoStock = 777;
+//		System.out.println(productoService.modificarStock(id, nuevoStock));
+
+//		Mostrar productos
+//		List<Producto> productos = productoService.productos();
+//		System.out.println("---------------   PRODUCTOS    ----------------");
+//		for (Producto producto : productos) {
+//			System.out.println(producto);
+//		}
+
+// ----------------------------- CLIENTE ----------------------------- 
+
+//		Register
+//		String documento = "42414241";
+//		String nombre = "Carlos Perez";
+//		String mail = "carlosperez@gmail.com";
+//		String password = "carlos";
+//		String direccion = "UADE 1234";
+//		System.out.println(clienteService.registerCliente(documento, nombre, mail, password, direccion));
+
+//		Eliminar Cliente
+//		String mail = "tomasdorigo@gmail.com";
+//		System.out.println(clienteService.eliminarCliente(mail));
+
+//		Recuperar Contrasenia Cliente
+//		String mail = "tomasdorigo@gmail.com";
+//		System.out.println(clienteService.recuperarContrasenaCliente(mail));
+
+//		Login Cliente
+//		String mail = "tomasdorigo@gmail.com";
+//		String password = "nacho";
+//		System.out.println(clienteService.loginCliente(mail, password));
+
+//		Cambiar contrasenia cliente
+//		String mail = "tomasdorigo@gmail.com";
+//		String password = "abcd";
+//		String nueva1 = "nacho";
+//		String nueva2 = "nacho";
+//		System.out.println(clienteService.cambiarContraseniaCliente(mail, password, nueva1, nueva2));
+
+//		Agregar Producto Carrito
+//		Recibe un mail, idProducto y Cantidad
+//		Existe mail --> Existe Producto --> Se crea detalle --> Se agrega a carrito y se guarda
+//		String mail = "pedro@gmail.com";
+//		Integer id = 2;
+//		Integer cantidad = 10;
+//		System.out.println(clienteService.agregarProductoCarrito(mail, id, cantidad));
+
+//		Mostrar Carrito
+		String mail = "pedro@gmail.com";
+		List<Detalle> carrito = clienteService.verCarrito(mail);
+		System.out.println(" TU CARRITO  ");
+		for (Detalle detalle : carrito) {
+			System.out.println(detalle.getProducto());
+			System.out.println(detalle.getCantidad());
+		}
+
+//		Vaciar Carrito
+//		String mail = "pedro@gmail.com";
+//		System.out.println(clienteService.vaciarCarrito(mail));
+		
+		
+//		Eliminar Producto Carrito
+//		String mail = "pedro@gmail.com";
+//		Integer id = 3;
+//		System.out.println(clienteService.eliminarProductoCarrito(mail, id));
+
+	}
+
+}
---
+++ tpomongoredis/src/main/java/com/example/demo/service/ClienteService.java
@@ -0,0 +1,224 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.modelo.Admin;
+import com.example.demo.modelo.Carrito;
+import com.example.demo.modelo.Cliente;
+import com.example.demo.modelo.Detalle;
+import com.example.demo.modelo.Producto;
+import com.example.demo.repository.ClienteRepository;
+import com.example.demo.repository.ProductoRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	ClienteRepository repositorio;
+
+	@Autowired
+	ProductoRepository productoRepository;
+
+	@Autowired
+	EmailSenderService emailSenderService;
+
+//	Register
+//	Eliminar Cliente
+//	Login
+//	Olvide contrasenia
+//	Cambiar Contrasenia
+//	Todos clientes
+
+//  Listo
+	public String registerCliente(String documento, String nombre, String mail, String password, String direccion) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isEmpty()) {
+			Cliente clienteNuevo = new Cliente(documento, nombre, mail, password, direccion);
+			repositorio.save(clienteNuevo);
+			emailSenderService.sendEmail("ignaciodorigo@gmail.com", "Registro en APP",
+					nombre + " te has registrado exitosamente en la app");
+			return "Registro exitoso";
+		} else {
+			return "Ese mail ya esta registrado";
+		}
+	}
+
+//	No hace falta endpoint
+	public String eliminarCliente(String mail) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			repositorio.deleteById(mail);
+			return "Cliente eliminado";
+		} else {
+			return "No existe ese cliente";
+		}
+	}
+
+//  Listo
+	public String loginCliente(String mail, String password) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			if (cliente.getPassword().equals(password)) {
+				return "Login exitoso";
+			} else {
+				return "Contrasena incorrecta";
+			}
+
+		} else {
+			return "Error no existe";
+		}
+	}
+
+//	Listo
+	public String recuperarContrasenaCliente(String mail) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			String contrasenia = cliente.getPassword();
+			emailSenderService.sendEmail("ignaciodorigo@gmail.com", "Recupero contrasenia en APP",
+					"Tu contrasenia es : " + contrasenia);
+			return "Envio de contrasenia al mail";
+		} else {
+			return "Error no existe";
+		}
+	}
+
+//	Listo endpoint falta visual
+	public String cambiarContraseniaCliente(String mail, String actual, String nueva1, String nueva2) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			String contrasenia = cliente.getPassword();
+			if (contrasenia.equals(actual)) {
+				if (nueva1.equals(nueva2)) {
+					cliente.setPassword(nueva1);
+					repositorio.save(cliente);
+					emailSenderService.sendEmail("ignaciodorigo@gmail.com", "Cambio contrasenia en APP",
+							"Has cambiado tu contrasenia, tu nueva contrasenia es: " + nueva1);
+					return "Cambio contrasenia exitoso";
+				} else {
+					return "Las contrasenias no coinciden";
+				}
+			} else {
+				return "Contrasenia actual incorrecta";
+			}
+		}
+		return "Error cliente no encontrado";
+	}
+
+//	Listo
+	public List<Cliente> clientes() {
+		return repositorio.findAll();
+	}
+
+//  Listo
+	public Cliente perfilCliente(String mail) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			return cliente;
+		} else {
+			return null;
+		}
+	}
+
+//	Ver Carrito
+	public List<Detalle> verCarrito(String mail) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			return cliente.getCarrito().getDetalles();
+		} else {
+			return null;
+		}
+	}
+
+//	Agregar Producto al carrito (Nuevo)
+	public String agregarProductoCarrito(String mail, Integer id, Integer cantidad) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+//		Validamos que exista el cliente
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			Optional<Producto> productoOptional = productoRepository.findById(id);
+//			Validamos que exista el producto
+			if (productoOptional.isPresent()) {
+				Producto p = productoOptional.get();
+				Carrito carritoCliente = cliente.getCarrito();
+//				Si esto devuelve true es porque encontro el producto en el carrito
+				if (carritoCliente.productoEnCarrito(p)) {
+
+//					Modificamos la cantidad
+					carritoCliente.modificarCantidadProducto(p, cantidad);
+					cliente.setCarrito(carritoCliente);
+					repositorio.save(cliente);
+					return "Stock modificado al carrito";
+
+				} else {
+//					Creamos un detalle y lo agregamos al carrito(lo seteamos y lo volvemos a guardar)
+					Detalle detalle = new Detalle(p, cantidad);
+					carritoCliente.agregarDetalleCarrito(detalle);
+					cliente.setCarrito(carritoCliente);
+					repositorio.save(cliente);
+					return "Producto agregado al carrito";
+
+				}
+
+			} else {
+				return "El ID del Producto no existe";
+			}
+		} else {
+			return "El cliente no existe";
+		}
+	}
+
+//	Eliminar Producto del carrito (FALTA)
+	public String eliminarProductoCarrito(String mail, Integer id) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+//		Validamos que exista el cliente
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			Optional<Producto> productoOptional = productoRepository.findById(id);
+//			Validamos que exista el producto
+			if (productoOptional.isPresent()) {
+				Producto producto = productoOptional.get();
+				Carrito carritoCliente = cliente.getCarrito();
+//				Preguntamos si el producto esta en el carrito
+				if (carritoCliente.productoEnCarrito(producto)) {
+					carritoCliente.eliminarDetalleCarrito(producto);
+					cliente.setCarrito(carritoCliente);
+					repositorio.save(cliente);
+					return "Producto eliminado del carrito";
+				} else {
+					return "El producto no esta en el carrito";
+				}
+			} else {
+				return "El producto no existe";
+			}
+
+		} else {
+			return "El cliente no existe";
+		}
+	}
+
+//	Vaciar Carrito
+	public String vaciarCarrito(String mail) {
+		Optional<Cliente> clienteOptional = repositorio.findById(mail);
+		if (clienteOptional.isPresent()) {
+			Cliente cliente = clienteOptional.get();
+			Carrito carritoVacio = new Carrito(new ArrayList<Detalle>());
+			cliente.setCarrito(carritoVacio);
+			repositorio.save(cliente);
+			return "Carrito vacio";
+		} else {
+			return "El cliente no existe";
+		}
+	}
+
+//	Confirmar carrito (Recibimos datos para la facutra, la creamos, y seteamos el carrito)
+}
